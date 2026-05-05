5a97532505d464aa7f050e34daf92bcfe17d7bdc
Rushikesh Dusane <rushikeshrd01@gmail.com>
2026-05-05 18:47:47 +0530

    redundant in vite.config.js
    using Vite.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-//uses vite
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
